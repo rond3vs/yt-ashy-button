b4786dd08ad2109dad43ef2bc84f570d74a0a332
rond3vs <rond3vs@gmail.com>
2026-03-01 22:07:44 -0600

    Add HTML, CSS, and JS files

---
+++ index.js
@@ -0,0 +1,6 @@
+const ashyBtn = document.querySelector('button');
+
+
+function checkAshyLevels() {
+    ashyBtn.innerText = 'Yes, go put some lotion on and refresh the page please. You ashy, dusty, crusty, bafoon!'
+}
